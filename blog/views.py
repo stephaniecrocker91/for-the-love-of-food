ced0ca824e6e3563596dc14d3efe7a15019cab0e
Stephie <stephaniecrocker91@gmail.com>
2022-07-14 14:01:50 +0000

    Install crispy forms and write code in views.py for comments submisions

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
 from .models import Recipe
+from .forms import CommentForm
+
 
 # Create your views here.
 
@@ -27,7 +29,38 @@ class RecipeDetail(View):
             "recipe_detail.html",
             {
                 "recipe": recipe,
-                "comments": comment, 
-                "liked": liked
+                "comments": comment,
+                # "commented": False, 
+                "liked": liked,
+                "comment_form": CommentForm()
+            },
+        )
+
+    def post(self, request, slug, *args, **kwargs):
+        queryset = Recipe.objects.filter(status=1)
+        recipe = get_object_or_404(queryset, slug=slug)
+        comment = recipe.comment.order_by('created_on')
+        liked = False
+        if recipe.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        comment_form = CommentForm(data=request.POST)
+
+        if comment_form.is_valid():
+            comment_form.instance.email = request.user.email
+            comment_form.instance.name = request.user.username
+            comment_form.save()
+        else:
+            comment_form = CommentForm()
+
+        return render(
+            request, 
+            "recipe_detail.html",
+            {
+                "recipe": recipe,
+                "comments": comment,
+                "commented": True,
+                "liked": liked,
+                "comment_form": CommentForm()
             },
         )
