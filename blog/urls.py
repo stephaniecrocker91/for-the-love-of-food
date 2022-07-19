2083d6d7404a076f6e8ba1a1d67f137ff291aa23
Stephie <stephaniecrocker91@gmail.com>
2022-07-19 21:37:03 +0000

    Debug post recipe by swapping order fo urls so that create recipe is before slug:slug

--- blog/urls.py
+++ blog/urls.py
@@ -7,7 +7,8 @@ urlpatterns = [
     path('', views.RecipeList.as_view(), name='home'),
     path('favourites/<slug:slug>', views.Favourite.as_view(), name='favourites'),
     path('favourites/', views.RecipeFavourites.as_view(), name='recipe_favourites'),
+    path('create_recipe/', CreateRecipeView.as_view(), name='create_recipe'),
     path('<slug:slug>/', views.RecipeDetail.as_view(), name='recipe_detail'),
     path('like/<slug:slug>', views.RecipeLike.as_view(), name='recipe_like'),
-    path('create_recipe/', CreateRecipeView.as_view(), name='create_recipe'),
+    # path('create_recipe/', CreateRecipeView.as_view(), name='create_recipe'),
 ]
