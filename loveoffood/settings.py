c4388f8e6c028035efd6f3e09cb9e9ae3d0cf68a
Stephie <stephaniecrocker91@gmail.com>
2022-07-26 14:42:01 +0000

    Style forms and create links for editing recipe with authorization and verification

--- loveoffood/settings.py
+++ loveoffood/settings.py
@@ -42,14 +42,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.sites',
-    'allauth',
-    'allauth.account',
-    'allauth.socialaccount',
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
-    'django_summernote',
     'crispy_forms',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'django_summernote',
     'blog',
 ]
 
